354126c7bdd8301ced2a16349feab114e1d216d7
micaelaalvaradomendez <micaelaalvaradomendez@gmail.com>
2026-04-21 12:16:24 -0300

    ultimo ejercicio de clojure resuelto

--- clojure/src/tp04/ej18.clj
+++ clojure/src/tp04/ej18.clj
@@ -3,21 +3,21 @@
 
 ;; {:ok true :value orden} si activa y total > 100. Error si no.
 (defn clasificar-orden [orden]
-  (cond (not (:activa? orden)) {:ok false  :error "orden inactiva"}
-    (<= (:total orden)100) {:ok false :error "monto insuficiente"}
-    :else {:ok true :value orden}))
+  (cond (not (:activa? orden)) {:ok false :error "orden inactiva"}
+        (<= (:total orden) 100) {:ok false :error "monto insuficiente"}
+        :else {:ok true :value orden}))
 
 ;; Retorna nueva orden con total reducido por porcentaje.
 (defn aplicar-descuento [porcentaje orden]
   (assoc orden :total (* (:total orden)
-    (-1 (/ porcentaje 100)))))
+                         (- 1 (/ porcentaje 100)))))
 
 ;; Pipeline: clasificar → separar → descuento 10% → sumar.
 ;; Retorna {:aprobadas [...] :rechazadas [...] :total-final N}
 (defn procesar-ordenes [ordenes]
-  (let [clasificadas (map calisificar-orden ordenes)
-    aprobadas (filter :ok clasificadas)
-    rechazadas (filter #(not (:ok %)) clasificadas)
+  (let [clasificadas (map clasificar-orden ordenes)
+        aprobadas (filter :ok clasificadas)
+        rechazadas (filter #(not (:ok %)) clasificadas)
         con-descuento (map #(aplicar-descuento 10 (:value %)) aprobadas)
         total-final (reduce + 0 (map :total con-descuento))]
     {:aprobadas (vec con-descuento)
